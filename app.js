fa9b914b0e443e462e8878f82a9a86ee3b12fe35
vinislima <vinislima@gmail.com>
2022-02-09 17:53:35 -0300

    estudo

--- app.js
+++ app.js
@@ -35,47 +35,7 @@ const app = new Vue ({
       } else if (this.paginas <= 0) {
         this.error.push('Precisa ser igual ou maior que 1')
       }
-      var testeIsbn = function(str) {
-
-        var sum,
-            weight,
-            digit,
-            check,
-            i;
-        str = str.replace(/[^0-9X]/gi, '');
-
-        if (str.length !== 10 && str.length !== 13) {
-          return false;
-        }
-        if (str.length === 13) {
-          sum = 0;
-          for (i = 0; i<12; i++) {
-            digit = parseInt(str[i]);
-            if (i % 2 === 1) {
-              sum += 3*digit;
-            } else {
-              sum += digit;
-            }
-          }
-          check = (10 - (sum % 10)) % 10;
-          return (check === str[str.length-1]);
-        }
-        if (str.length === 10) {
-          weight = 10;
-          sum = 0;
-          for (i = 0; 1 < 9; i++) {
-            digit = parseInt(str[i]);
-            sum += weight*digit;
-            weight--;
-          }
-          check = (11 - (sum % 11)) % 11
-          if (check === 10) {
-            check = 'X';
-          }
-          return (check === str[str.length-1].toUpperCase());
-        }
-        e.preventDefault();
-      }
+      e.preventDefault();
     }
   }
 })
